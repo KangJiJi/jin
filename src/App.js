7866ae3337843a26e1df045336f8e2147100ae58
KangJi <kjhmd9726@gmail.com>
2024-03-13 00:09:43 +0900

    deploy

--- src/App.js
+++ src/App.js
@@ -3,8 +3,88 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div>
-        jin
+      <div className="wrapper">
+        <div className="scroll-text">
+          <h1>Hello Jin</h1>
+          <h2>진님 안녕하세요</h2>
+          <p>오늘도 10시 쯤 퇴근했어....</p>
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <p>술 조금만 드시고</p>
+          <p>책 읽으시고</p>
+          <p>맨날 슬프다고 하지 마시고</p>
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <p>나 대학생때 하던거랑</p>
+          <p>많이 달라져서 공부중 ㅋㅋ</p>
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <p>담번에는 더 재미있는거</p>
+          <p>만들어줄께</p>
+        </div>
       </div>
     </div>
   );
