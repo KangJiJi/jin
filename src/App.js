5c1a904cc87402ff4eb204ca09405bdda52c0492
KangJi <kjhmd9726@gmail.com>
2024-03-12 23:03:07 +0900

    deploy

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        jin
+      </div>
+    </div>
+  );
+}
+
+export default App;
--- src/app/page.js
+++
@@ -1,9 +0,0 @@
-import styles from "./page.module.css";
-
-export default function Home() {
-  return (
-    <div className={styles.main}>
-        <h1>Hello Jin</h1>
-    </div>
-  );
-}
