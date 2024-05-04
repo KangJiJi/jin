11c66ef047da9fabd8128a36e9c304536fe3f7ab
KangJi <kjhmd9726@gmail.com>
2024-05-05 00:22:07 +0900

    app router 기반, main 페이지 구현

---
+++ app/main/page.js
@@ -0,0 +1,9 @@
+export default function Main() {
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <div>
+        <div>Here is main page</div>
+      </div>
+    </main>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -1,9 +1,19 @@
+'use client';
+
+import Button from '@mui/material/Button';
+
 export default function Home() {
+  const routeToMainPage = () => {
+    console.log('Hello world');
+  };
+
   return (
     <main className="flex min-h-screen items-center justify-center">
       <div>
         <div>logo</div>
-        <div>button</div>
+        <Button variant="contained" onClick={routeToMainPage}>
+          Start one choice
+        </Button>
       </div>
     </main>
   );
