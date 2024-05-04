e7cef06f0813eff30eca3c80a43c5afb62496f69
KangJi <kjhmd9726@gmail.com>
2024-05-05 02:35:06 +0900

    image import and center align

--- app/choiceone/page.js
+++ app/choiceone/page.js
@@ -1,8 +1,17 @@
+import Image from 'next/image';
+import IconVs from '/public/icons/vs.png';
+
 export default function ChoiceOne() {
   return (
     <main className="min-w-dvw w-screen min-h-dvh h-screen">
-      <div className="w-full h-1/2 bg-blue-500">up</div>
-      <div className="w-full h-1/2 bg-green-500">down</div>
+      <div className="w-full h-12">Title</div>
+      <div className="relative w-full h-[calc(100%-3rem)]">
+        <div className="w-full h-1/2 bg-blue-500">up</div>
+        <div className="w-full h-1/2 bg-green-500">down</div>
+        <div className="absolute top-1/2 left-1/2 w-20 h-20 -ml-10 -mt-10 z-10000">
+          <Image className="w-full" src={IconVs} alt="vs" priority />
+        </div>
+      </div>
     </main>
   );
 }
