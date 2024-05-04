54e2ad0f96886e0597179fc685fe2444038244db
KangJi <kjhmd9726@gmail.com>
2024-05-05 01:27:01 +0900

    div full

--- app/choiceone/page.js
+++ app/choiceone/page.js
@@ -1,9 +1,8 @@
 export default function ChoiceOne() {
   return (
-    <main className="flex min-h-screen items-center justify-center">
-      <div>
-        <div>Here is ChoiceOne page</div>
-      </div>
+    <main className="min-w-dvw w-screen min-h-dvh h-screen">
+      <div className="w-full h-1/2 bg-blue-500">up</div>
+      <div className="w-full h-1/2 bg-green-500">down</div>
     </main>
   );
 }
