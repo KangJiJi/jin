be73fe3d0c0091ff0d879b22a21a240a7ff6831f
KangJi <kjhmd9726@gmail.com>
2024-05-06 02:02:01 +0900

    choice page layout 구성

--- app/choiceone/page.js
+++ app/choiceone/page.js
@@ -4,7 +4,13 @@ import Image from 'next/image';
 import IconVs from '/public/icons/vs.png';
 import Profile from '/components/profile/Profile';
 
+import ProfileKeke from '/public/profile/keke.png';
+
 export default function ChoiceOne() {
+  const handleSelect = () => {
+    console.log('clicked!');
+  };
+
   return (
     <main className="min-w-dvw w-screen min-h-dvh h-screen">
       <div className="w-full h-12 flex justify-center items-center">
@@ -12,10 +18,10 @@ export default function ChoiceOne() {
       </div>
       <div className="relative w-full h-[calc(100%-3rem)]">
         <div className="w-full h-1/2 bg-blue-500 flex justify-center items-center">
-          <Profile />
+          <Profile name="zzzzzz" onClick={handleSelect} />
         </div>
         <div className="w-full h-1/2 bg-green-500 flex justify-center items-center">
-          <Profile />
+          <Profile image={ProfileKeke} name="zzzz" onClick={handleSelect} />
         </div>
         <div className="absolute top-1/2 left-1/2 w-20 h-20 -ml-10 -mt-10 z-1000">
           <Image className="w-full" src={IconVs} alt="vs" priority />
--- components/profile/profile.js
+++ components/profile/profile.js
@@ -1,15 +1,28 @@
 import Image from 'next/image';
 import Button from '@mui/material/Button';
-import IconProfile from '/public/icons/profile.png';
+import ProfileDefault from '/public/profile/default.png';
 
-export default function Profile() {
+export default function Profile({ image, name, onClick }) {
   return (
-    <div className="w-50 aspect-[1.618/1]">
-      <div className="w-full mb-3 flex justify-center">
-        <Image className="w-40" src={IconProfile} alt="vs" priority />
+    <div className="w-52 aspect-[1.618/1]">
+      <div className="w-full mb-1 flex justify-center items-center">
+        <div className="w-44">
+          <Image
+            className="w-full"
+            src={image ? image : ProfileDefault}
+            alt="profile image"
+          />
+        </div>
+      </div>
+      <div className="w-full mb-2 flex justify-center items-center">
+        <div>{name}</div>
       </div>
       <div className="w-full flex justify-center items-center">
-        <Button variant="contained" className="aspect-[1.618/1]">
+        <Button
+          variant="contained"
+          className="aspect-[1.618/1]"
+          onClick={onClick}
+        >
           Select
         </Button>
       </div>
