da446e2f67f99b89ff8fb3a60abc782592e84770
KangJi <kjhmd9726@gmail.com>
2024-05-08 01:11:18 +0900

    선택 애니메이션 구현

--- app/choiceone/page.js
+++ app/choiceone/page.js
@@ -1,5 +1,6 @@
 'use client';
 
+import { useState } from 'react';
 import Image from 'next/image';
 import IconVs from '/public/icons/vs.png';
 import Profile from '/components/profile/Profile';
@@ -7,25 +8,73 @@ import Profile from '/components/profile/Profile';
 import ProfileKeke from '/public/profile/keke.png';
 
 export default function ChoiceOne() {
-  const handleSelect = () => {
+  const [topButtonClicked, setTopButtonClicked] = useState(false);
+  const [bottomButtonClicked, setBottomButtonClicked] = useState(false);
+
+  const handleSelect = (buttonClickedSetter, selectedProfile) => {
+    buttonClickedSetter(true);
     console.log('clicked!');
   };
 
   return (
-    <main className="min-w-dvw w-screen min-h-dvh h-screen">
+    <main className="min-w-dvw w-screen min-h-dvh h-screen font-sans">
       <div className="w-full h-12 flex justify-center items-center">
-        <div>Title</div>
+        <div className="text-2xl font-bold">Title</div>
       </div>
       <div className="relative w-full h-[calc(100%-3rem)]">
-        <div className="w-full h-1/2 bg-blue-500 flex justify-center items-center">
-          <Profile name="zzzzzz" onClick={handleSelect} />
+        <div
+          className={`w-full bg-blue-500 flex justify-center items-center h-1/2`}
+        >
+          <div
+            className={`${
+              bottomButtonClicked
+                ? 'transition duration-1000 ease-in-out opacity-0'
+                : ''
+            } ${
+              topButtonClicked
+                ? 'transition duration-1000 ease-in-out translate-y-full'
+                : ''
+            }`}
+          >
+            <Profile
+              name="zzzzzz"
+              onClick={() => handleSelect(setTopButtonClicked)}
+              buttonVisibility={topButtonClicked}
+            />
+          </div>
         </div>
-        <div className="w-full h-1/2 bg-green-500 flex justify-center items-center">
-          <Profile image={ProfileKeke} name="zzzz" onClick={handleSelect} />
+
+        <div
+          className={`w-full bg-green-500 flex justify-center items-center h-1/2`}
+        >
+          <div
+            className={`${
+              topButtonClicked
+                ? 'transition duration-1000 ease-in-out opacity-0'
+                : ''
+            } ${
+              bottomButtonClicked
+                ? 'transition duration-1000 ease-in-out -translate-y-full'
+                : ''
+            }`}
+          >
+            <Profile
+              image={ProfileKeke}
+              name="zzzz"
+              onClick={() => handleSelect(setBottomButtonClicked)}
+              buttonVisibility={bottomButtonClicked}
+            />
+          </div>
         </div>
-        <div className="absolute top-1/2 left-1/2 w-20 h-20 -ml-10 -mt-10 z-1000">
-          <Image className="w-full" src={IconVs} alt="vs" priority />
-        </div>
+
+        {topButtonClicked == bottomButtonClicked && (
+          <>
+            <div className="absolute top-1/2 border-4 border-black w-full z-999"></div>
+            <div className="absolute top-1/2 left-1/2 w-20 h-20 -ml-10 -mt-10 z-1000">
+              <Image className="w-full" src={IconVs} alt="vs" priority />
+            </div>
+          </>
+        )}
       </div>
     </main>
   );
--- components/profile/profile.js
+++ components/profile/profile.js
@@ -2,7 +2,7 @@ import Image from 'next/image';
 import Button from '@mui/material/Button';
 import ProfileDefault from '/public/profile/default.png';
 
-export default function Profile({ image, name, onClick }) {
+export default function Profile({ image, name, buttonVisibility, onClick }) {
   return (
     <div className="w-52 aspect-[1.618/1]">
       <div className="w-full mb-1 flex justify-center items-center">
@@ -15,16 +15,18 @@ export default function Profile({ image, name, onClick }) {
         </div>
       </div>
       <div className="w-full mb-2 flex justify-center items-center">
-        <div>{name}</div>
+        <div className="font-medium">{name}</div>
       </div>
       <div className="w-full flex justify-center items-center">
-        <Button
-          variant="contained"
-          className="aspect-[1.618/1]"
-          onClick={onClick}
-        >
-          Select
-        </Button>
+        {!buttonVisibility && (
+          <Button
+            variant="contained"
+            className="aspect-[1.618/1]"
+            onClick={onClick}
+          >
+            Select
+          </Button>
+        )}
       </div>
     </div>
   );
